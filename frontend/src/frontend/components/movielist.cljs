f82473d45d5e42b6a970d17075ef7848ef126e78
Marcus Schreiter <marcus.schreiter@meso.design>
2019-06-20 12:43:39 +0200

    render edit button, watched and rating

--- frontend/src/frontend/components/movielist.cljs
+++ frontend/src/frontend/components/movielist.cljs
@@ -1,21 +1,39 @@
 (ns frontend.components.movielist
   (:require [frontend.state :as state]))
 
-(defn row [movie]
-  (let [movieData (:movie movie)]
-    [:tr
-     [:td (:titel movieData)]
-     [:td (:director movieData)]
-     [:td (:link movieData)]
-     [:td (:notes movieData)]
-     [:td (:rating movieData)]
-     [:td (:watched movieData)]]))
+
+(defn renderLink [link]
+  [:a {:href link, :target "_blank"} link])
+
+(defn renderEditButton [id]
+  [:button {:id id} "Edit"])
+
+(defn renderWatched [watched]
+  (if watched
+    [:span "yes"]
+    [:span "no"]))
+
+(defn renderRating [rating watched]
+  (if watched
+    [:span rating]
+    [:span "-"]))
+
+(defn renderRow [movie]
+  (let [{:keys [_id titel director link notes rating watched]} (:movie movie)]
+    [:tr {:key _id}
+     [:td titel]
+     [:td director]
+     [:td (renderLink link)]
+     [:td notes]
+     [:td (renderRating rating watched)]
+     [:td (renderWatched watched)]
+     [:td (renderEditButton _id)]]))
 
 (defn movielist
   []
   [:main
    [:table.watchlist
-    [:thead nil
-     [:tr nil '([:th "Titel"] [:th "Director"] [:th "Watch Link"] [:th "Notes"] [:th "My Rating"] [:th "Watched"])]]
-    [:tbody nil
-     (map row @state/movies)]]])
+    [:thead
+     [:tr [:th "Titel"] [:th "Director"] [:th "Watch Link"] [:th "Notes"] [:th "My Rating"] [:th "Watched"] [:th "Edit"]]]
+    [:tbody
+     (map renderRow @state/movies)]]])
