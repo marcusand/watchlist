8d152d6ff3b0184dac2691b2dd36afe93392e0e6
Marcus Schreiter <marcus.schreiter@meso.design>
2019-06-20 16:22:44 +0200

    implement form submit handle

--- frontend/src/frontend/components/addmovie.cljs
+++ frontend/src/frontend/components/addmovie.cljs
@@ -1,13 +1,28 @@
-(ns frontend.components.addmovie)
+(ns frontend.components.addmovie
+  (:require [reagent.core :as r]
+            [frontend.api :as api]
+            [clojure.string :as s]))
+
+(def newMovieTitel (r/atom nil))
+(def newMovieDirector (r/atom nil))
+(def newMovieLink (r/atom nil))
+(def newMovieNotes (r/atom nil))
+
+(defn handleAddMovie []
+  (if (s/blank? @newMovieTitel)
+    (prn "no titel")
+    (let [newMovie
+          {:titel @newMovieTitel, :director @newMovieDirector, :link @newMovieLink, :notes @newMovieNotes}]
+      (api/postMovie newMovie))))
 
 (defn addmovie
   []
   [:div
    [:h2 "Add a movie:"]
    [:div#add-movie-form
-    [:input {:placeholder "movie titel"}]
-    [:input {:placeholder "director"}]
-    [:input {:placeholder "watch links"}]
-    [:input {:placeholder "notes"}]
-    [:button "Add to list"]]
+    [:input {:placeholder "movie titel" :on-change #(reset! newMovieTitel (-> % .-target .-value))}]
+    [:input {:placeholder "director" :on-change #(reset! newMovieDirector (-> % .-target .-value))}]
+    [:input {:placeholder "watch links" :on-change #(reset! newMovieLink (-> % .-target .-value))}]
+    [:input {:placeholder "notes" :on-change #(reset! newMovieNotes (-> % .-target .-value))}]
+    [:button {:on-click handleAddMovie} "Add to list"]]
    [:hr]])
